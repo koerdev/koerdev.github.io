b04a61cb3fc93ab660ff0453712ad15ea22f04cc
David Cornejo <koerdev@gmail.com>
2026-04-22 04:22:56 -0400

    fix: mobile menu and add vite config

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+const btn = document.querySelector('#menu-btn');
+const menu = document.querySelector('#mobile-menu');
+
+let open = false;
+
+btn.addEventListener('click', () => {
+  open = !open;
+
+  if (open) {
+    menu.classList.remove('max-h-0', 'opacity-0');
+    menu.classList.add('max-h-96', 'opacity-100');
+  } else {
+    menu.classList.add('max-h-0', 'opacity-0');
+    menu.classList.remove('max-h-96', 'opacity-100');
+  }
+});
